e467ed8be5f667181aa91dded73571a33d4ed789
rina <rina23>
2020-12-18 11:22:48 +0900

    ini commit

---
+++ action.js
@@ -0,0 +1,21 @@
+
+$(function(){
+
+$('#good1').click(function(){
+
+
+$('#good1').text('💕Thank you💕').css('font-size','20px').css('color','white').css('width','300px');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+});
